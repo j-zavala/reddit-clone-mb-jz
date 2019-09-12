1421c1a66bf145e6e461031eb15ea1bfab2bacf9
Johnny Zavala <jzavalafgz@gmail.com>
2019-09-12 11:34:16 -0400

    test

--- js/home.js
+++ js/home.js
@@ -20,50 +20,35 @@ function postData(event) {
     })
     .then(res => {
       localStorage.setItem("user", res.token);
-      logUserIn();
+      signupUser();
     })
     .catch(err => {
       console.log(err);
     });
 }
 
-function logUserIn() {
+function signupUser() {
   let token = localStorage.getItem("user");
   if (token) {
-    document.querySelector(".signupForm").style.display = "none";
+    // document.querySelector(".signupForm").style.display = "none";
+    // document.querySelector(".loginForm").style.display = "none";
+    document.querySelector(".accountOptions").style.display = "none";
     document.querySelector(".postForm").style.display = "flex";
     document.querySelector(".commentForm").style.display = "flex";
-
-    fetch("http://thesi.generalassemb.ly:8080/user/post", {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("user")
-      }
-    })
-      .then(res => {
-        return res.json();
-      })
-      .then(res => {
-        console.log(res);
-        //         const list = document.querySelector('.posts');
-        // ​
-        //         for (let i = 0; i < res.length; i++) {
-        //             const item = document.createElement('li');
-        //             const title = document.createElement('h3');
-        //             const description = document.createElement('p');
-        //             item.appendChild(title);
-        //             item.appendChild(description);
-        //             title.innerText = res[i].title;
-        //             description.innerText = res[i].description;
-        //             list.appendChild(item);
-        //         }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  } else if (token === null) {
-    console.log("error");
   }
 }
+
+// function logUserIn() {
+//   let token = localStorage.getItem("user");
+//   if (token) {
+//     document.querySelector(".signupForm").style.display = "none";
+//     document.querySelector(".postForm").style.display = "flex";
+//     document.querySelector(".commentForm").style.display = "flex";
+//     createPost();
+//   } else if (token === null) {
+//     console.log("error");
+//   }
+// }
 
 // function updateDom() {
 //   document.querySelector(".signupForm").style.display = "none";
@@ -96,27 +81,29 @@ function logUserIn() {
 //     });
 // }
 
-// //Allow user to post after login
-// function createPost(event) {
-//   // event.preventDefault();
-//   const title = document.querySelector(".title");
-//   const description = document.querySelector(".description");
-//   fetch("http://thesi.generalassemb.ly:8080/post", {
-//     method: "POST",
-//     headers: {
-//       Authorization: "Bearer " + localStorage.getItem("user"),
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//       title: title.value,
-//       description: description.value
-//     })
-//   })
-//     .then(res => {
-//       console.log(res);
-//       updateDom(res);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// }
+//Allow user to post after login
+function createPost(event) {
+  event.preventDefault();
+  const title = document.querySelector(".title");
+  const description = document.querySelector(".description");
+  fetch("http://thesi.generalassemb.ly:8080/post", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("user"),
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      title: title.value,
+      description: description.value
+    })
+  })
+    .then(res => {
+      console.log(res);
+      // updateDom(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+document.querySelector(".postForm").addEventListener("click", createPost);
