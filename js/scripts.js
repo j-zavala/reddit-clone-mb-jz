b74012f203200d79b80fe767f20c26631a64c5f1
Johnny Zavala <jzavalafgz@gmail.com>
2019-09-12 11:36:07 -0400

    hello

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,3 @@
-// require("dotenv").config();
-
-// require syntax
-// const Unsplash = require("unsplash-js").default;
-
-// const unsplash = new Unsplash({
-//   applicationId: process.env.APP_ACCESS_KEY,
-//   secret: process.env.APP_SECRET
-// });
-
-// console.log(userAuthentication(code));
-
-// export function userAuthentication(code) {
-//   return unsplash.auth
-//     .userAuthentication(code)
-//     .then(toJson)
-//     .then(json => json.access_token);
-// }
 window.onload = function() {
   let id = "0984c448918c7679173694ae2737ab63ea485e7e738715dd5818ad1ab1b20752";
 
@@ -32,7 +14,6 @@ window.onload = function() {
       let viewport = document.getElementsByClassName("container")[0];
       //console.log(viewport[0]);
       viewport.style.backgroundImage = `url(${res.urls.regular})`;
-      console.log;
     })
     .catch(err => {
       console.log(err);
